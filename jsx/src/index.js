5ff6cee498af1a668cf6bca48c9a05a13e5e456c
Kumar Sandeep Ramanath <kumarsandeepr89@gmail.com>
2021-02-22 21:38:06 +0530

    Adding code to JSX

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,5 +1,23 @@
 // Import the React and ReactDOM library
+import React from "react";
+import ReactDOM from "react-dom";
 
 // Create a react Component
-
+const App = function () {
+  //   return <div> Hi there !</div>;
+  const buttonText = "Click Me";
+  return (
+    <div>
+      <label for="name" className="label">
+        Enter Name
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText}
+      </button>
+    </div>
+  );
+};
 // Take the react component and show it on screen
+
+ReactDOM.render(<App />, document.querySelector("#root"));
