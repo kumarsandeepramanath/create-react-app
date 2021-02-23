5f97ddbeb89e727d4e6581f16f1512df97676665
Kumar Sandeep Ramanath <kumarsandeepr89@gmail.com>
2021-02-23 21:23:13 +0530

    Conditional looping and using setState

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,17 +2,40 @@ import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
-  return (
-    <div>
-      <div> Hi There</div>
-      <SeasonDisplay></SeasonDisplay>
-    </div>
-  );
-};
+// const App = () => {
+
+//   return (
+//     <div>
+//       <div> Hi There</div>
+//       <SeasonDisplay></SeasonDisplay>
+//     </div>
+//   );
+// };
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    //THIS IS THE ONLY TIME WE DO DIRECT ASSIGNMENT to this.state
+    this.state = { lat: null, errorMessage: "" };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div> Error : {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div> Latitude : {this.state.lat}</div>;
+    }
+    return <div> Loading!!</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
