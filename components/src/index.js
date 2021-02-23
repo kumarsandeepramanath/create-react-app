23b28f3ac48ba077d3f5b41f63f2d82587091416
Kumar Sandeep Ramanath <kumarsandeepr89@gmail.com>
2021-02-23 13:10:30 +0530

    ApprovedCard component

--- components/src/index.js
+++ components/src/index.js
@@ -2,28 +2,37 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 import CommentDetails from "./CommentDetail";
-
+import ApprovalCard from "./ApprovalCard";
 const App = function () {
   return (
     <div className="container ui comments">
-      <CommentDetails
-        author="Sam"
-        timeAgo="Today at 4:54 pm"
-        comment="Nice Blog"
-        avatar={faker.image.image()}
-      />
-      <CommentDetails
-        author="Alex"
-        timeAgo="Today at 2:00 pm"
-        comment="Intersting"
-        avatar={faker.image.image()}
-      />
-      <CommentDetails
-        author="Jane"
-        timeAgo="Today at 9:15 am"
-        comment="Kudos for the effort"
-        avatar={faker.image.image()}
-      />
+      <ApprovalCard>
+        <CommentDetails
+          author="Sam"
+          timeAgo="Today at 4:54 pm"
+          comment="Nice Blog"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetails
+          author="Alex"
+          timeAgo="Today at 2:00 pm"
+          comment="Intersting"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetails
+          author="Jane"
+          timeAgo="Today at 9:15 am"
+          comment="Kudos for the effort"
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <div>Are you sure ?</div>
+      </ApprovalCard>
     </div>
   );
 };
