4b037375c43f43e95c343e0695185094c8820712
Kumar Sandeep Ramanath <kumarsandeepr89@gmail.com>
2021-02-23 16:14:09 +0530

    Functional Component

---
+++ seasons/src/SeasonDisplay.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SeasonDisplay = () => {
+  return <div> This is a Season Display</div>;
+};
+
+export default SeasonDisplay;
---
+++ seasons/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+
+const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (err) => console.log(err)
+  );
+  return (
+    <div>
+      <div> Hi There</div>
+      <SeasonDisplay></SeasonDisplay>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
