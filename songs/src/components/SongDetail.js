198c7d002577b619e1d523cd0ab321ef2a0fbc65
Kumar Sandeep Ramanath <kumarsandeepr89@gmail.com>
2021-03-11 13:46:27 +0530

    Song class based component 5

--- songs/src/components/SongDetail.js
+++ songs/src/components/SongDetail.js
@@ -9,7 +9,7 @@ class SongDetail extends React.Component {
         <h1> {songTitle.length > 0 ? `Song title is  - ${songTitle}` : ''}</h1>
         <h1>
           {' '}
-          {songDuration.length > 0 ? `Song Duration is ${songDuration}` : ''}
+          {songDuration.length > 0 ? `Song Duration is - ${songDuration}` : ''}
         </h1>
       </div>
     );
