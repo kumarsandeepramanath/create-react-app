5dfcbe02dafda38200b1d0985df8af0bc947c910
Kumar Sandeep Ramanath <kumarsandeepr89@gmail.com>
2021-03-15 23:21:57 +0530

    Redux additions

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,7 +1,19 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = () => {
-  return async (dispatch) => {
+// export const fetchPosts = () => {
+//   return async (dispatch) => {
+//     //Return a function instead of an Object
+//     const response = await jsonPlaceholder.get('/posts');
+//     dispatch({
+//       // If we using Thunk, we will dispatch an action object
+//       type: 'FETCH_POSTS',
+//       payload: response,
+//     });
+//   };
+// };
+
+export const fetchPosts = () =>  async dispatch => { // Function returning a Function
+    //Return a function instead of an Object
     const response = await jsonPlaceholder.get('/posts');
     dispatch({
       type: 'FETCH_POSTS',
@@ -9,3 +21,4 @@ export const fetchPosts = () => {
     });
   };
 };
+
