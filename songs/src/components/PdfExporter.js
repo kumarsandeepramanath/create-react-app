0af78c4b2a6208e6c1e417fc375ca295275a4a04
Kumar Sandeep Ramanath <kumarsandeepr89@gmail.com>
2021-04-21 08:07:32 +0530

    Pdf Exporter - Autotable referene issues does not exist

--- songs/src/components/PdfExporter.js
+++ songs/src/components/PdfExporter.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
 
 var columns = [
   { title: 'A', dataKey: 'A' },
@@ -11,36 +13,43 @@ var rows = [
   { A: 'A', B: 'B', C: 'C' },
 ];
 
-// function createPdf() {
-//   var doc = new jsPDF('p', 'pt');
-//   doc.setFontSize(20);
-//   doc.setTextColor(40);
-//   doc.setFontStyle('normal');
-//   doc.text('YOLO', 10, 20);
+function createPdf() {
+  console.log('Inside createPDF');
+  var doc = new jsPDF('p', 'pt');
+  doc.setFontSize(20);
+  doc.setTextColor(40);
+  //doc.setFontStyle('normal');
+  doc.text('YOLO', 10, 20);
 
-//   doc.autoTable(columns, rows, {
-//     startY: doc.autoTableEndPosY() + 70,
-//     margin: { horizontal: 10 },
-//     styles: { overflow: 'linebreak' },
-//     bodyStyles: { valign: 'top' },
-//     columnStyles: { email: { columnWidth: 'wrap' } },
-//     theme: 'striped',
-//   });
+  doc.autoTable(columns, rows, {
+    startY: doc.autoTableEndPosY() + 70,
+    margin: { horizontal: 10 },
+    styles: { overflow: 'linebreak' },
+    bodyStyles: { valign: 'top' },
+    columnStyles: { email: { cellWidth: 'wrap' } },
+    theme: 'striped',
+  });
 
-//   doc.autoTable(columns, rows, {
-//     startY: doc.autoTableEndPosY() + 70,
-//     margin: { horizontal: 10 },
-//     styles: { overflow: 'linebreak' },
-//     bodyStyles: { valign: 'top' },
-//     columnStyles: { email: { columnWidth: 'wrap' } },
-//     theme: 'striped',
-//   });
-//   doc.save('repro.pdf');
-// }
+  doc.autoTable(columns, rows, {
+    startY: doc.autoTableEndPosY() + 70,
+    margin: { horizontal: 10 },
+    styles: { overflow: 'linebreak' },
+    bodyStyles: { valign: 'top' },
+    columnStyles: { email: { cellWidth: 'wrap' } },
+    theme: 'striped',
+  });
+  doc.save('repro.pdf');
+}
 
 class PdfExporter extends React.Component {
   render() {
-    return <div>Pdf Exporter</div>;
+    return (
+      <div>
+        Pdf Exporter Autotable issue does not exist here
+        <br />
+        <button onClick={createPdf}>Click to Download pdf</button>
+      </div>
+    );
   }
 }
 
