143e0dddfed25cf96a8bd9363bb9c741b9d2c79d
Kumar Sandeep Ramanath <kumarsandeepr89@gmail.com>
2021-04-23 23:49:45 +0530

    Pdf changes4

--- songs/src/components/PdfExporter.js
+++ songs/src/components/PdfExporter.js
@@ -47,7 +47,7 @@ class PdfExporter extends React.Component {
       <div>
         Pdf Exporter Autotable issue does not exist here.
         <br />
-        <button onClick={createPdf}>Download PDF</button>
+        <button onClick={createPdf}>Click to Download PDF</button>
       </div>
     );
   }
